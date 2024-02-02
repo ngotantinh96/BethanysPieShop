8784d4c4bd05bda457427688b81701f47106ec9e
tinhngo-saigontechnology <tinh.ngo@saigontechnology.com>
2024-02-02 15:47:09 +0700

    feat: implement list pies page

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,24 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository pieRepository;
+        private readonly ICategoryRepository categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            this.pieRepository = pieRepository;
+            this.categoryRepository = categoryRepository;
+        }
+        public IActionResult List()
+        {
+            ViewBag.CurrentCategory = "Cheese cakes";
+            var pieListViewModel = new PieListViewModel(pieRepository.AllPies, "Cheese cakes");
+            return View(pieListViewModel);
+        }
+    }
+}
--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,6 +1,11 @@
+using BethanysPieShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Service registration
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
