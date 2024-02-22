67ab9a704442b4bf29d06cf8923efe812cba2b6b
tinhngo-saigontechnology <tinh.ngo@saigontechnology.com>
2024-02-22 11:45:19 +0700

    feate: Implement UnitTest for PieController.List() using xUnit and Moq

---
+++ BethanysPieShop.Tests/Controllers/PieControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using BethanysPieShop.Controllers;
+using BethanysPieShop.ViewModels;
+using BethanysPieShopTests.Mocks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Tests.Controllers
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void List_EmptyCategory_ReturnsAllPies()
+        {
+            //Arrange
+            var mockCategoryRepository = RepositoryMocks.GetCategoryRepository();
+            var mockPieRepository = RepositoryMocks.GetPieRepository();
+            var pieController = new PieController(mockPieRepository.Object, mockCategoryRepository.Object);
+
+            //Act
+            var result = pieController.List(string.Empty);
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+    }
+}
